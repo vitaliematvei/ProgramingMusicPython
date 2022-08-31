c9d47c0d2bdad9664b8abcdd49f9de10ce9f65f0
Matvei Vitalie <vitaliematvei@gmail.com>
2022-08-31 16:46:01 +0300

    Changhed to grid and finish show result

--- Transposition/main.py
+++ Transposition/main.py
@@ -7,11 +7,25 @@ class Window(tk.Tk):
         self.geometry("450x150")
         self.resizable(False, False)
         self.title("Transposition")
+        self.grid_columnconfigure((0, 1), weight=1)
+
+        # TOP LABEL
         self.label_text = tk.StringVar()
         self.label_text.set("Enter base note (use # for sharps, eg. A#): ")
         self.name_text = tk.StringVar()
         self.label = tk.Label(self, textvar=self.label_text, font=("Arial", 11, "bold"))
-        self.label.pack(anchor="w", padx=10, pady=5)
+        self.label.grid(row=1, column=0, padx=10, pady=5)
+
+        # TOP INPUT
+        self.note_text = tk.StringVar()
+        self.note_entry = tk.Entry(self, textvariable=self.note_text)
+        self.note_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        # SHOW RESULT
+        self.label_result = tk.StringVar()
+        self.label_result = self.note_entry
+        self.label_result_entry = tk.Label(self, textvariable=self.label_result, font=("Arial", 11, "bold"))
+        self.label_result_entry.grid(row=3, column=1, padx=5, pady=5)
 
 
 if __name__ == "__main__":
