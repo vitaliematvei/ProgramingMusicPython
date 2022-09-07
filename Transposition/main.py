752f6cfb8b682bcead2304927b8f586c9ccf444c
Matvei Vitalie <vitaliematvei@gmail.com>
2022-09-07 16:35:33 +0300

    created button and show label

--- Transposition/main.py
+++ Transposition/main.py
@@ -9,26 +9,48 @@ class Window(tk.Tk):
         self.title("Transposition")
         self.grid_columnconfigure((0, 1), weight=1)
 
-        # TOP LABEL
-        self.label_text = tk.StringVar()
-        self.label_text.set("Enter base note (use # for sharps, eg. A#): ")
-        self.name_text = tk.StringVar()
-        self.label = tk.Label(self, textvar=self.label_text, font=("Arial", 11, "bold"))
+        # NOTE INPUT LABEL
+        self.top_label_text = tk.StringVar()
+        self.top_label_text.set("Enter base note (use # for sharps, eg. A#): ")
+        self.label = tk.Label(self, textvar=self.top_label_text, font=("Arial", 11, "bold"))
         self.label.grid(row=1, column=0, padx=10, pady=5)
 
-        # TOP INPUT
+        # NOTE INPUT
         self.note_text = tk.StringVar()
         self.note_entry = tk.Entry(self, textvariable=self.note_text)
         self.note_entry.grid(row=1, column=1, padx=5, pady=5)
 
-        # SHOW RESULT
-        self.label_result = tk.StringVar()
-        self.label_result = self.note_entry
-        self.label_result_entry = tk.Label(self, textvariable=self.label_result, font=("Arial", 11, "bold"))
-        self.label_result_entry.grid(row=3, column=1, padx=5, pady=5)
+        # INTERVAL INPUT LABEL
+        self.interval_label_text = tk.StringVar()
+        self.interval_label_text.set("Enter interval in semitones: ")
+        self.interval_label = tk.Label(self, textvar=self.interval_label_text, font=("Arial", 11, "bold"))
+        self.interval_label.grid(row=2, column=0, pady=5)
+
+        # INTERVAL INPUT
+        self.interval_text = tk.StringVar()
+        self.interval_entry = tk.Entry(self, textvariable=self.interval_text)
+        self.interval_entry.grid(row=2, column=1, padx=5, pady=5)
+
+        # ---------------------Calculate Button ---------------------
+        self.transpose_button = tk.Button(self, text="Calculate", command=self.transpose)
+        self.transpose_button.grid(row=3, column=1, padx=5, pady=5)
+
+        # ---------------------Calculate Interval Label ---------------------
+        self.show_label_text = tk.StringVar()
+        self.show_label_text.set("Transposed by x is : ")
+        self.show_label_value = tk.StringVar()
+        self.show_label = tk.Label(self, textvar=self.show_label_value, font=("Arial", 11, "bold"))
+        self.show_label.grid(row=4, column=0, padx=10, pady=5)
+
+
+    def transpose(self):
+        #show = translate_from_note_to_pitch_class(
+         #   self.note_entry.get().lower(),
+          #  self.interval_entry.get().lower())
+        show = self.note_entry.get().lower() + self.interval_entry.get().lower()
+        self.show_label_value.set(show)
 
 
 if __name__ == "__main__":
     window = Window()
     window.mainloop()
-
